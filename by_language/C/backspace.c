af4e6a9b2df002e3c085ded072b8aad0eae00e21
Jonathan Price <jon.mark.price@gmail.com>
2016-12-04 19:26:56 -0600

    experiments with \r and \b

---
+++ by_language/C/backspace.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+   printf("a b c");
+
+   // Goes back to beginning
+   printf("\b");
+   printf("d\n");
+}
---
+++ by_language/C/carriage-ret.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+   printf("a b c");
+
+   // Goes back to beginning
+   printf("\r");
+   printf("A\n");
+}
