78317d2153fc924415049c0bdb62b4e71d261189
Jonathan Price <jon.mark.price@gmail.com>
2016-12-30 00:14:32 -0600

    renamed scala to Scala, added another hello world

---
+++ by_language/Scala/hello_obj.scala
@@ -0,0 +1,8 @@
+// The "official" Hello World program from:
+// http://docs.scala-lang.org/tutorials/scala-for-java-programmers.html
+
+object HelloWorld {
+  def main(args: Array[String]) {
+    println("Hello, world!")
+  }
+}
