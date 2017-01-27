76680900f7513fd6c1f53e0e33b12761a3c30935
Jonathan Price <jon.mark.price@gmail.com>
2017-01-26 22:28:13 -0600

    implemented slices, checked that set() param is correct size

--- by_language/Python/bits/bits.py
+++ by_language/Python/bits/bits.py
@@ -1,4 +1,4 @@
-class Bits():
+class BitArray():
 
     def __init__(self, n):
         # TODO test length non-neg
@@ -10,11 +10,14 @@ class Bits():
         return self.length
 
     def set(self, array):
+        if len(array) != self.length:
+            raise BadSet()
+
         self.data = array
 
     def array(self):
         if self.data is None:
-            raise Bits.NoValue()
+            raise NoValue()
         else:
             return self.data
 
@@ -29,7 +32,7 @@ class Bits():
         #    # Or default to 0
 
         if self.data is None:
-            raise Bits.NoValue()
+            raise NoValue()
         else:
             exp   = self.length - 1
             value = 0
@@ -38,5 +41,47 @@ class Bits():
                 exp -= 1
             return value
 
-    class NoValue(Exception):
-        pass
+class NoValue(Exception):
+    pass
+
+# TODO rename
+# TODO split into Incomplete and Overflow
+class BadSet(Exception):
+    pass
+
+class Bits(BitArray):
+    # should this be the base class?...
+
+    # Can you have a slice of a slice? No. Unneccessary.
+    # But, we could have Bit have a slice() method
+
+    def slice(self, low, high):
+        '''
+        Get a slice of bits that is still mapped back to the main object.
+        '''
+        # Can't just return a slice...
+        # Can't return a new Bit object either, as this would make a
+        # copy... right?
+
+        new = Slice(low, high, self)
+        new.data = self.data[low:high + 1]
+        return new
+
+class Slice(BitArray):
+    # Has more adv. set method
+    def __init__(self, low, high, parent):
+        self.parent = parent
+        self.low    = low
+        self.high   = high
+        self.size   = 1 + high - low 
+
+    def set(self, array):
+        if len(array) != self.size:
+            raise BadSet()
+
+        # rebinds:
+        # self.data = array
+        for i in list(range(0, len(array))):
+            self.parent.data[i] = array[i]
+         
+
--- by_language/Python/bits/tests.py
+++ by_language/Python/bits/tests.py
@@ -1,4 +1,5 @@
 import unittest
+import bits
 from bits import Bits
 
 class BitTests(unittest.TestCase):
@@ -17,23 +18,38 @@ class BitTests(unittest.TestCase):
         a.clear()
         self.assertEqual(a.array(), [0, 0, 0])
 
+        # TODO: an set() of the wrong size should raise an exception
+        with self.assertRaises(bits.BadSet): # XXX better name?
+            a.set([0])
+        with self.assertRaises(bits.BadSet):    # different for underflow
+            a.set([0, 0, 0, 0])                 # and overflow?
+
     # Consider initiating content
     def test_unset(self):
         # Should raise NoValue if size of 1+
-        with self.assertRaises(Bits.NoValue):
+        with self.assertRaises(bits.NoValue):
             Bits(1).array()
 
         # Should not raise NoValue for size of 0
         self.assertEqual(Bits(0).array(), [])
         self.assertEqual(Bits(0).num(), 0)
 
-        with self.assertRaises(Bits.NoValue):
+        with self.assertRaises(bits.NoValue):
             Bits(1).num()
-
         
     def test_num(self):
-        pass
+        a = Bits(3)
+        a.set([1, 1, 0])
+        self.assertEqual(a.num(), 6)
 
+    def test_slice(self):
+        # I want a way to be able to get a slice of bits and set
+        # them, which should update the whole object.
+        a = Bits(5)
+        a.clear()
+        b = a.slice(0, 3) # inclusive (first 4 bits)
+        b.set([0, 1, 1, 0])
+        self.assertEqual(a.array(), [0, 1, 1, 0, 0])
 
 if __name__ == '__main__':
     unittest.main()
