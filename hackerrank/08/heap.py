ddb8eb674969c750f76f80f84ff4b1c7b24eefdf
Jonathan Price <jon.mark.price@gmail.com>
2017-03-03 20:11:07 -0600

    more work on heap

--- hackerrank/08/heap.py
+++ hackerrank/08/heap.py
@@ -1,71 +1,93 @@
 # Following a "by-example" approach
+dbg = False
 
 class Heap():
+    #def __init__(self, n):
+    #    assert n > 0
+    #    self.array  = [0] * n
+    #    self.start  = n - 1
+    #    self.END    = n - 1 # Constant
+        
     def __init__(self, array=[]):
         self.array = array
 
-    def heapify(self, index=0):
-        if self.array == []:
-            return
+    def reheap(self, verbose=dbg):
+        last  = len(self.array) - 1
+        index = 0
         
-        left  = 2 * index + 1
-        right = 2 * index + 2
-        parent = (index - 1) // 2
-        value = self.array[index]
-        last  = self.size() - 1
-        assert last >= index
+        while index < last:
+            left  = 2 * index + 1
+            right = 2 * index + 2
+            parent = (index - 1) // 2
+            value = self.array[index]
 
-        # Compare to childen
-        if last >= right: # Both children are there
-            if value < min(self.array[left], self.array[right]):
-                # Check both children
-                self.heapify(left)
-                self.heapify(right)
-            elif self.array[left] < self.array[right]:
-                # Swap and back to sqaure #1
-                self.print_array(index)
-                print('swap {} and {}'.format(value, self.array[left]))
-                self.swap(index, left)
-                if index != 0:
-                    self.heapify(parent)
+            # Compare to childen
+            if right <= last: # Both children are there
+                if value <= min(self.array[left], self.array[right]):
+                    # No swap necessary, the heap property is in place.
+                    if verbose:
+                        print("Heap property restored.")
+                    #return
+                    index += 1
+
+                elif self.array[left] < self.array[right]:
+                    # We swapped with the smaller element, therefore, we only 
+                    # need to check the swapped element's children
+                    if verbose:
+                        self.print_array(index)
+                        print('Swap {} and {}'.format(value, self.array[left]))
+                    self.swap(index, left)
+                    index = left
+
                 else:
-                    self.heapify(index)
+                    # Swap and back to sqaure #1
+                    if verbose:
+                        self.print_array(index)
+                        print('Swap {} and {}'.format(value, self.array[right]))
+                    self.swap(index, right)
+                    index = right
+
+            # Special cases: 
+            elif left == last:
+                # Only one child
+                if value <= self.array[left]:
+                    if verbose:
+                        print("Heap property restored.")
+                    return
+                else:
+                    if verbose:
+                        self.print_array(index)
+                        print('Swap {} and {}'.format(value, self.array[left]))
+                    self.swap(index, left)
+                    #index = left
+                    return # this was the last child
             else:
-                # Swap and back to sqaure #1
-                self.print_array(index)
-                print('swap {} and {}'.format(value, self.array[right]))
-                self.swap(index, right)
-                if index != 0:
-                    self.heapify(parent)
-                else:
-                    self.heapify(index)
-        elif last == left:
-            # Only one child
-            if value > self.array[left]:
-                self.print_array(index)
-                print('swap {} and {}'.format(value, self.array[left]))
-                self.swap(index, left)
-                if index != 0:
-                    self.heapify(parent)
-                else:
-                    self.heapify(index)
-        else:
-            self.print_array(index)
-            print('OK.')
+                # Last element
+                return
 
     def swap(self, x, y):
         self.array[x], self.array[y] = self.array[y], self.array[x]
 
     def add(self, value):
-        self.array.append(value)
+        if self.size() == 0:
+            self.array.append(value)
+        else:
+            self.array.append(self.array[0])
+            self.array[0] = value
+            self.reheap()
 
-    def pop(self):
+    def pop(self, verbose=False):
         if self.array == []:
             return None
+        if verbose:
+            print(self.array)
         popped = self.array[0]
+        
+        # Replace with last element, or itself if len == 1, then pop
         self.array[0] = self.array[-1]
-        self.array.pop()
-        self.heapify()
+        self.array.pop() # or del
+        
+        self.reheap()
         return popped
         
     def size(self):
@@ -79,37 +101,34 @@ class Heap():
         s += ', '.join(str(self.array[i]) for i in range(index + 1, n))
         s += ']'
         print(s, end='')
-
-def even(n):
-    if n % 2 == 0:
-        return True
-    else:
-        return False
+        
+    def sort(self):
+        in_order = []
+        n = self.size()
+        for i in range(0, n):
+            in_order.append(self.pop())
+        self.array = in_order
 
 def median(array):
     n = len(array)
-    if even(n):
+    if n % 2 == 0:
         return (array[(n // 2) - 1] + array[n // 2]) / 2.0
     else:
         return float(array[n // 2])
 
-def heapsort(heap):
-    in_order = []
-    n = heap.size()
-    heap.heapify()
-    for i in range(0, n):
-        in_order.append(heap.pop())
-    return in_order
-
-
-if __name__ == '__name__':
-    h = Heap()
-
+if __name__ == '__main__':
     n = int(input().strip())
-    for _ in range(n):
-        item = int(input().strip())
-        h.add(item)
-        tmp = Heap(h.array[:])
-        #print(median(tmp))
-        print(heapsort(tmp))
-
+    h = Heap()
+    a = []
+    a_i = 0
+    for a_i in range(n):
+        a_t = int(input().strip())
+        a.append(a_t)
+        h.add(a_t)
+        h.sort()
+        if True:
+            print('input  : ' + str(a))
+            print('sorted : ' + str(h.array))
+        #print(median(h.array))
+    if dbg:
+        print("Input was: " + str(a))
